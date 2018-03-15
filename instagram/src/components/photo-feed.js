9046df157b6c96e8dd94e843bf64b452995b6dcb
msintaha <msintaha94@gmail.com>
2018-03-15 20:15:12 +0600

    fix indent

--- instagram/src/components/photo-feed.js
+++ instagram/src/components/photo-feed.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
-import { connect } from 'react-redux';
-import PhotoSection from './photo-section';
-import { getPhotos } from '../actions';
+import React, { Component } from "react";
+import { View, ScrollView } from "react-native";
+import { connect } from "react-redux";
+import PhotoSection from "./photo-section";
+import { getPhotos } from "../actions";
 
 class PhotoFeed extends Component {
-	componentWillMount() {
-		this.props.getPhotos();
-	}
-
-  renderFeed() {
-  	return this.props.photos.map(photo => {
-  		return (
-  			<PhotoSection key={photo.id} photo={photo} />
-  		);
-  	});
+  componentWillMount() {
+    this.props.getPhotos();
   }
 
-	render() {
-		return <ScrollView>{this.renderFeed()}</ScrollView>
-	}
-};
+  renderFeed() {
+    return this.props.photos.map(photo => {
+      return <PhotoSection key={photo.id} photo={photo} />;
+    });
+  }
 
-const mapStateToProps = (state) => {
-	return { photos: state.photos };
+  render() {
+    return <ScrollView>{this.renderFeed()}</ScrollView>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { photos: state.photos };
 };
 
 export default connect(mapStateToProps, { getPhotos })(PhotoFeed);
