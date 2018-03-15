26d355ac54ee552a7d92e843c8cfc76b7ebb4665
msintaha <msintaha94@gmail.com>
2018-03-15 20:16:06 +0600

    fix indent

--- instagram/src/components/header.js
+++ instagram/src/components/header.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 
 class Header extends Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.text}>{this.props.title}</Text>
-			</View>
-		);
-	}
-};
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.props.title}</Text>
+      </View>
+    );
+  }
+}
 
 const styles = {
-	container: {
-		justifyContent: 'flex-start',
+  container: {
+    justifyContent: "flex-start",
     paddingTop: 15,
-    alignItems: 'center',
-    backgroundColor: '#F8F8F8',
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 1},
+    alignItems: "center",
+    backgroundColor: "#F8F8F8",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.2
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     margin: 15
   }
 };
